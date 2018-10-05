7ea1063050a67e1b0d1b47d89b1aeeb1f451de6a
unknown <c.kunapakornkarn@hotmail.com>
2018-10-05 13:21:39 +0700

    update

---
+++ src/main/java/com/dto/TypeDto.java
@@ -0,0 +1,18 @@
+package com.dto;
+
+public class TypeDto {
+	String id;
+	String name;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
