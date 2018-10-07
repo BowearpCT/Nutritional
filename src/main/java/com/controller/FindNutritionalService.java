92ffeb85933668f8a95f4a756509a3ee8d769076
PraewoB <c.kunapakornkarn@hotmail.com>
2018-10-07 15:50:48 +0700

    find nutritional success

--- src/main/java/com/controller/FindNutritionalService.java
+++ src/main/java/com/controller/FindNutritionalService.java
@@ -1,20 +1,25 @@
 package com.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-
-
 import org.bson.Document;
 import org.modelmapper.ModelMapper;
+
 
 import com.connect.Connect;
 import com.dto.MasterDto;
 import com.google.gson.Gson;
+import com.google.common.collect.Iterables;
 import com.google.gson.JsonObject;
+import com.mongodb.BasicDBObject;
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 
 @Path("/nutritional")
@@ -26,10 +31,32 @@ public class FindNutritionalService {
 		Connect mongo = new Connect();
 		JsonObject message = new JsonObject();
 		Gson gson = new Gson();
-		MongoCollection<Document> collection = mongo.db.getCollection("solution");
+		MongoCollection<Document> collection = mongo.db.getCollection("master");
 		ModelMapper Mapper = new ModelMapper();
 		
+		BasicDBObject query = new BasicDBObject();
+		List<BasicDBObject> obj = new ArrayList<BasicDBObject>();
+		obj.add(new BasicDBObject("thname", java.util.regex.Pattern.compile(masterDto.getThname())));
+		obj.add(new BasicDBObject("enname", java.util.regex.Pattern.compile(masterDto.getEnname())));
+		query.put("$or", obj);
 		
+		
+		MasterDto[] value = null;
+		
+		try {
+			FindIterable<Document> data = collection.find(query);
+			int size = Iterables.size(data);
+			value = new MasterDto[size];
+			int key = 0;
+			for (Document document : data) {
+				value[key++] = Mapper.map(document, MasterDto.class);
+			}
+			message.addProperty("message", true);
+		}catch (Exception e) {
+			message.addProperty("message", false);
+		}finally {
+			message.add("data", gson.toJsonTree(value));
+		}
 		
 		return Response.ok(gson.toJson(message), MediaType.APPLICATION_JSON).build();
 	} 
