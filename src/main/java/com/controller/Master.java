bc1bdf5328c485da5c0258b03ea3f7b80210bc44
PraewoB <c.kunapakornkarn@hotmail.com>
2018-10-15 16:39:40 +0700

    can update

--- src/main/java/com/controller/Master.java
+++ src/main/java/com/controller/Master.java
@@ -53,7 +53,33 @@ public class Master {
 		
 		return Response.ok(gson.toJson(message),MediaType.APPLICATION_JSON).build();
 	}
-	
+	@POST
+	@Path("/findOne")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response findOne(MasterDto masterDto) {
+		Connect mongo = new Connect();
+		JsonObject message = new JsonObject();
+		Gson gson = new Gson();
+		MongoCollection<Document> collection = mongo.db.getCollection("master");
+		ModelMapper Mapper = new ModelMapper();
+		
+		BasicDBObject searchQuery = new BasicDBObject();
+		searchQuery.put("_id", masterDto.getId());
+		
+		MasterDto value = new MasterDto();
+		
+		try {
+			FindIterable<Document> data = collection.find(searchQuery);
+			value = Mapper.map(data.first(), MasterDto.class);
+			message.addProperty("message", true);
+		}catch (Exception e) {
+			message.addProperty("message", false);
+		}finally {
+			message.add("data", gson.toJsonTree(value));
+		}
+		
+		return Response.ok(gson.toJson(message), MediaType.APPLICATION_JSON).build();
+	}
 	@POST
 	@Path("/findAll")
 	@Consumes(MediaType.APPLICATION_JSON)
@@ -91,30 +117,33 @@ public class Master {
 		Connect mongo = new Connect();
 		JsonObject message = new JsonObject();
 		Gson gson = new Gson();
-		MongoCollection<Document> collection = mongo.db.getCollection("solution");
+		MongoCollection<Document> collection = mongo.db.getCollection("master");
+		ModelMapper Mapper = new ModelMapper();
+		MasterDao masterDao = Mapper.map(masterDto, MasterDao.class);
 		
-		MasterDao masterDao = new MasterDao();
-		masterDao.setThname(masterDto.getThname());
-		masterDao.setEnname(masterDto.getEnname());
-		masterDao.setType(masterDto.getType());
-		masterDao.composition.setEnergy(masterDto.composition.getEnergy());
-		masterDao.composition.setWater(masterDto.composition.getWater());
-		masterDao.composition.setProtein(masterDto.composition.getProtein());
-		masterDao.composition.setFat(masterDto.composition.getFat());
-		masterDao.composition.setCarbohydrate(masterDto.composition.getCarbohydrate());
-		masterDao.composition.setFiber(masterDto.composition.getFiber());
-		masterDao.composition.setAsh(masterDto.composition.getAsh());
-		masterDao.mineral.setCalsium(masterDto.mineral.getCalsium());
-		masterDao.mineral.setIron(masterDto.mineral.getIron());
-		masterDao.mineral.setPhosphorus(masterDto.mineral.getPhosphorus());
-		masterDao.vitamin.setRetinol(masterDto.vitamin.getRetinol());
-		masterDao.vitamin.setBetacarotene(masterDto.vitamin.getBetacarotene());
-		masterDao.vitamin.setVitaminA(masterDto.vitamin.getVitaminA());
-		masterDao.vitamin.setVitaminE(masterDto.vitamin.getVitaminE());
-		masterDao.vitamin.setThiamin(masterDto.vitamin.getThiamin());
-		masterDao.vitamin.setRiboflavin(masterDto.vitamin.getRiboflavin());
-		masterDao.vitamin.setNiacin(masterDto.vitamin.getNiacin());
-		masterDao.vitamin.setVitaminC(masterDto.vitamin.getVitaminC());
+		
+//		MasterDao masterDao = new MasterDao();
+//		masterDao.setThname(masterDto.getThname());
+//		masterDao.setEnname(masterDto.getEnname());
+//		masterDao.setType(masterDto.getType());
+//		masterDao.composition.setEnergy(masterDto.composition.getEnergy());
+//		masterDao.composition.setWater(masterDto.composition.getWater());
+//		masterDao.composition.setProtein(masterDto.composition.getProtein());
+//		masterDao.composition.setFat(masterDto.composition.getFat());
+//		masterDao.composition.setCarbohydrate(masterDto.composition.getCarbohydrate());
+//		masterDao.composition.setFiber(masterDto.composition.getFiber());
+//		masterDao.composition.setAsh(masterDto.composition.getAsh());
+//		masterDao.mineral.setCalsium(masterDto.mineral.getCalsium());
+//		masterDao.mineral.setIron(masterDto.mineral.getIron());
+//		masterDao.mineral.setPhosphorus(masterDto.mineral.getPhosphorus());
+//		masterDao.vitamin.setRetinol(masterDto.vitamin.getRetinol());
+//		masterDao.vitamin.setBetacarotene(masterDto.vitamin.getBetacarotene());
+//		masterDao.vitamin.setVitaminA(masterDto.vitamin.getVitaminA());
+//		masterDao.vitamin.setVitaminE(masterDto.vitamin.getVitaminE());
+//		masterDao.vitamin.setThiamin(masterDto.vitamin.getThiamin());
+//		masterDao.vitamin.setRiboflavin(masterDto.vitamin.getRiboflavin());
+//		masterDao.vitamin.setNiacin(masterDto.vitamin.getNiacin());
+//		masterDao.vitamin.setVitaminC(masterDto.vitamin.getVitaminC());
 
 		String json = gson.toJson(masterDao);
 		Document document = Document.parse(json);
